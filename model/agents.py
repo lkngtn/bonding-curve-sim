moved bonding curve logic out of model, and fixed gini calculation

model/agents.py
@@ -5,22 +5,23 @@ class Trader(Agent):
     '''
     The Trader class implements standard actions for an agent to interact with a single currency/token market model.
     '''
-    def __init__(self, unique_id, model, currency, tokens):
+    def __init__(self, unique_id, model, market, currency, tokens):
         super().__init__(unique_id, model)
         self.currency = currency
         self.tokens = tokens
         self.wealth = 1
+        self.market = market
 
     def calculate_wealth(self):
-        self.wealth = self.currency + (self.tokens * self.model.current_price())
+        self.wealth = self.currency + (self.tokens * self.market.current_price())
 
     def buy(self,amount):
-        self.tokens += self.model.buy(amount)
+        self.tokens += self.market.buy(amount)
         self.currency -= amount
 
     def sell(self,amount):
         self.tokens -= amount
-        self.currency += self.model.sell(amount)
+        self.currency += self.market.sell(amount)
 
     def skip(self):
         pass
@@ -29,7 +30,7 @@ class Trader(Agent):
         '''
         Implement agent strategy by inheriting class and overwritting this function.
         '''
-        skip()
+        self.skip()
 
     def step(self):
         self.calculate_wealth()
@@ -38,17 +39,16 @@ class Trader(Agent):
 
 class RandomTrader(Trader):
     '''
-    This trader implements a trading strategy based on a random belief of token value.
+    This trader implements a strategy based on a random belief of token value.
     '''
-    def __init__(self, unique_id, model, currency, tokens, belief_sigma, trade_percent):
-        super().__init__(unique_id, model, currency, tokens)
+    def __init__(self, unique_id, model, market, currency, tokens, belief_sigma):
+        super().__init__(unique_id, model, market, currency, tokens)
         self.sigma = belief_sigma
-        self.trade_percent = trade_percent
 
     def step_strategy(self):
-        self.current_price = self.model.current_price()
-        self.value_beleif = np.random.normal(self.current_price, self.sigma)
-        if self.value_beleif < self.current_price and self.tokens > 0:
-            self.sell(self.tokens * self.trade_percent)
+        self.current_price = self.market.current_price()
+        self.value_belief = np.random.normal(0, self.sigma)
+        if self.value_belief < 0 and self.tokens > 0:
+            self.sell(self.tokens * self.value_belief)
         elif self.currency > 0:
-            self.buy(self.currency*self.trade_percent)
+            self.buy(self.currency * self.value_belief)

model/model.py
@@ -1,59 +1,70 @@
 import random
 import math
 import mesa
+import numpy as np
 
 from mesa import Model
 from mesa.datacollection import DataCollector
 from mesa.time import RandomActivation
 
 from model.agents import RandomTrader
+from lib.gini import gini
 
 def compute_gini(model):
-    agent_wealths = [agent.wealth for agent in model.schedule.agents]
-    x = sorted(agent_wealths)
-    N = model.num_agents
-    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
-    return (1 + (1 / N) - 2 * B)
+    agent_wealths = np.array([float(agent.wealth) for agent in model.schedule.agents])
+    return gini(agent_wealths)
 
-class BondingCurve(Model):
+def compute_price(model):
+    return model.market.current_price()
+
+class BondingCurve():
     '''
-        A market model that manages balances of a token and currency pair based on a pricing curve determined by a ratio.
+        A class for bonding curve based token economies
     '''
-    def __init__(self, num_agents, token_supply, ratio, agent_starting_currency, agent_belief_sigma, agent_trade_percent):
-        super().__init__()
-        self.num_agents = num_agents
+    def __init__(self, token_supply, ratio):
         self.token_supply = token_supply
         self.ratio = ratio
         self.reserve = self.token_supply * self.ratio
 
-        self.schedule = RandomActivation(self)
-
-
-        # Instatiate agents
-        for i in range(self.num_agents):
-            a = RandomTrader(i, self, agent_starting_currency, (self.token_supply/self.num_agents), agent_belief_sigma, agent_trade_percent)
-            self.schedule.add(a)
-        self.running = True
-        self.datacollector = DataCollector(
-            model_reporters={"Gini": compute_gini},
-            agent_reporters={"Wealth": "wealth"}
-        )
-        self.datacollector.collect(self)
-
     def buy(self, amount):
-        self.purchase_amount = self.token_supply * ((1 + amount/self.reserve)**self.ratio -1)
+        self.purchase_amount = self.token_supply * (((1 + amount/self.reserve)**self.ratio) -1)
         self.token_supply += self.purchase_amount
         self.reserve += amount
+        print("Action Buy -- Reserve: " + str(self.reserve) + " Supply: " + str(self.token_supply) + " Ratio: " + str(self.ratio) + " Price :" + str(self.reserve / (self.token_supply * self.ratio)))
         return self.purchase_amount
 
     def sell(self, amount):
         self.sell_amount = self.reserve * (1 - (1- amount/self.token_supply)**(1/self.ratio))
         self.token_supply -= amount
         self.reserve -= self.sell_amount
+        print("Action: Sell -- Reserve: " + str(self.reserve) + " Supply: " + str(self.token_supply) + " Ratio: " + str(self.ratio) + " Price :" + str(self.reserve / (self.token_supply * self.ratio)))
         return self.sell_amount
 
     def current_price(self):
         return self.reserve / (self.token_supply * self.ratio)
+
+
+class RandomMarket(Model):
+    '''
+        A simulation of random trading behavior against a bonding curve based market.
+    '''
+    def __init__(self, num_agents, token_supply, ratio, agent_starting_currency, agent_belief_sigma):
+        super().__init__()
+        self.num_agents = num_agents
+        self.market = BondingCurve(token_supply, ratio)
+
+        self.schedule = RandomActivation(self)
+
+        # Instatiate agents
+        for i in range(self.num_agents):
+            a = RandomTrader(i, self, self.market, agent_starting_currency, (self.market.token_supply/self.num_agents), agent_belief_sigma)
+            self.schedule.add(a)
+        self.running = True
+        self.datacollector = DataCollector(
+            model_reporters={"Gini": compute_gini, "Price": compute_price},
+            agent_reporters={"Wealth": "wealth"}
+        )
+        self.datacollector.collect(self)
 
     def step(self):
         self.schedule.step()